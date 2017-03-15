668a6b224f25b87dc8a70f513bfde88a6ced01c2
Mike Erickson <codedungeon@gmail.com>
2017-03-15 09:21:18 -0700

    code update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 5.5 on Digital Ocean
                 </div>
 
                 <div class="links">
